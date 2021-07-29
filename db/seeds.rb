b9d24228ea2df1f2fc58e389ab9fe8a1c18f9406
Lpatch77 <81212050+Lpatch77@users.noreply.github.com>
2021-07-29 20:01:47 +1000

    added seeds_data file to seeds, added products.csv file, added require csv method to seeds, raked db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+CSV.foreach(Rails.root.join("db/seeds_data/products.csv"), headers: true) do |row|
+  Product.find_or_create_by(product: row[0], type: row[1], maker: row[2], location: row[3], price: row[4], desciption: row[5])
+end
